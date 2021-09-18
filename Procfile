1b761d2a4d2a9088897b1e7afc592935ce4e6ee9
Tareq <tssaad.g@gmail.com>
2021-09-19 01:45:07 +0200

    add environment variabls

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py makemigration --no-input
-release: python manage.py migrate--no-input
+release: python manage.py migrate --no-input
 web: gunicorn project.wsgi
--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wp97a&-xix21(w_hesi5f&l6mtbqcwp+p6tv06=z5qd0!x(brx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
